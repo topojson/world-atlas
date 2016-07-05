62ae037602c34b3271239a0bda5114fd35fe704c
Stan Dyro <standyro@gmail.com>
2016-07-05 14:55:19 -0700

    not europe countries

---
+++ not_european_countries.sql
@@ -0,0 +1,1 @@
+iso_a2 NOT IN ('XK', 'RU', 'GB', 'DZ','AD','BG','BY','FR','GI','IE','IL','JO','LB','LI','LU','MA','MC','MD','MK','MT','PS','PT','SM','SY','TN','TR','ME','RS','UK','CY','SJ','BE','NL','HR','CH','VA','PL','HU','ES','AT','DK','DE','AX','SE','UA','IT','AL','RO','FI','LV','EE','NO','CZ','FO','SK','SI','LT','IS','GR','BA')
